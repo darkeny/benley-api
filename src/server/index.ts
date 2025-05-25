fdafd698c05136ca84ef874cf29ca01f65fd8486
darkeny <darkenpaulo00@gmail.com>
2025-05-25 14:30:23 +0200

    update: adding preflights in the server with cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,33 +11,14 @@ const app = express();
 const prisma = new PrismaClient();
 const origins = process.env.KNOWN_ORIGINS?.split(",") || [];
 
-// LOG para debug
-app.use((req, res, next) => {
-    console.log("Incoming origin:", req.headers.origin);
-    console.log("Allowed origins:", origins);
-    next();
-});
-
-// Trata OPTIONS (preflight) corretamente
-app.options('*', cors());
-
-// Middleware CORS completo
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || origins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.error('Blocked by CORS:', origin);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: origins,
+    methods: '*', // Permitir todos os métodos
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 ServerRouters(app);
 
